91402aad05f23eae266a7ed905b775c6c8cbd9b3
dat <snapeandcandy@gmail.com>
2024-09-09 22:44:36 +0700

    generate OpenAPI

--- drf_project/urls.py
+++ drf_project/urls.py
@@ -17,10 +17,22 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(
+        'api/schema/',
+        SpectacularAPIView.as_view(permission_classes=[AllowAny], authentication_classes=[]),
+        name='schema',
+    ),
+    path(
+        'api/docs/',
+        SpectacularSwaggerView.as_view(url_name='schema', permission_classes=[AllowAny], authentication_classes=[]),
+        name='swagger-ui',
+    ),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include('expense_tracker.urls')),
