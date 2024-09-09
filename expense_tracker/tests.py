cbbdec9d1b8f8b3d9dfd102272be2c76e95746aa
dat <snapeandcandy@gmail.com>
2024-09-09 21:41:50 +0700

    init simple JWT

--- expense_tracker/tests.py
+++ expense_tracker/tests.py
@@ -1,1 +1,22 @@
 # Create your tests here.
+
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+class JWTAuthTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.token = RefreshToken.for_user(self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token.access_token}')
+
+    def test_expense_list_authenticated(self):
+        response = self.client.get(reverse('expense-list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_expense_list_unauthenticated(self):
+        self.client.credentials()  # Remove authentication
+        response = self.client.get(reverse('expense-list'))
+        self.assertEqual(response.status_code, 401)
