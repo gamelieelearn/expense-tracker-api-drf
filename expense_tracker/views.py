4d878ade163a0f64b2751801923ba6de1221081f
dat <snapeandcandy@gmail.com>
2024-09-09 21:34:03 +0700

    init DRF

--- expense_tracker/views.py
+++ expense_tracker/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from rest_framework import generics
+
+from .models import Expense
+from .serializes import ExpenseSerializer
+
+
+class ExpenseList(generics.ListCreateAPIView):
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
+
+
+class ExpenseDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
